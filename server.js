dbbceac61a8e5b227d89a09a3809b1658bd084f1
Hasti S. Gor <hastigor123@gmail.com>
2026-06-29 22:50:43 +0530

    Refactor server.js for database connection path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-const connectDB = require("./config/db");
+const connectDB = require("./db");
 
 dotenv.config();
 connectDB();
